d77679505f5d8dcbb64de6a9f98cb8d815f1db28
Rav1Rav1 <rakr7702@gmail.com>
2025-06-13 16:18:25 +0530

    dockerfile updated

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
 # Use Maven to build the app inside the container
-FROM maven:3.8.5-openjdk-17 AS build
+FROM maven:3.9.6-eclipse-temurin-23 AS build
 WORKDIR /app
 COPY . .
 RUN mvn clean package -DskipTests
 
-# Use OpenJDK to run the built JAR
-FROM openjdk:17-jdk-slim
+# Use OpenJDK 23 to run the built JAR
+FROM eclipse-temurin:23-jdk-jammy
 WORKDIR /app
 COPY --from=build /app/target/*.jar app.jar
 EXPOSE 8080
--- src/main/java/com/movieTicket/movieTicket/repository/UserRepository.java
+++ src/main/java/com/movieTicket/movieTicket/repository/UserRepository.java
@@ -4,7 +4,10 @@ package com.movieTicket.movieTicket.repository;
 
 import com.movieTicket.movieTicket.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     User findByEmail(String email);
 }
